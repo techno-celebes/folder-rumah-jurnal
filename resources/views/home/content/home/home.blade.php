860fda9c117f3a47352d7d386dc5698385c2bb4d
Techno Celebes <official@techno-celebes.com>
2022-08-19 16:03:36 +0800

    May be final

--- resources/views/home/content/home/home.blade.php
+++ resources/views/home/content/home/home.blade.php
@@ -44,4 +44,119 @@
 			</div>
 		</div>
 	</div>
+	<main class="container-fluid">
+		<div class="row">
+			<div class="col-12">
+				<div class="container main-wrapper">
+					<div class="row">
+						<div class="col-12">
+							<h3>Journals</h3>
+						</div>
+					</div>
+					<div class="row main-wrapper-parnot">
+						<div class="col-12">
+							<p>At UIN Datokarama Palu, we currently publish more than 380 peer-reviewed academic journals covering subjects across the humanities, social sciences and science, technology and medicine.</p>
+						</div>
+					</div>
+					<div class="row filter-wrapper">
+						<div class="col-sm-8 col-12">
+							<div class="row">
+								<div class="col-4">
+									<div class="input-group mb-3">
+										<span class="input-group-text" id="basic-addon1"><i class="fa fa-search"></i></span>
+										<input type="text" class='form-control' placeholder="Searchy And Filter">
+									</div>
+								</div>
+								<div class="col-4">
+									<select class="form-control">
+										<option value="1">All titles</option>
+									</select>
+								</div>
+								<div class="col-4">
+									<select class="form-control">
+										<option value="1">All Subjects</option>
+									</select>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4 col-12">
+							<div class="filter">
+								<p>Unfiltered</p>
+							</div>
+						</div>
+					</div>
+					<div class="row journal-list">
+						<div class="col-12">
+							<ul>
+								<li>
+									<div class="row">
+										<div class="col-sm-8 col-12 journal-name">
+											<a href="#">Hunafa</a>
+										</div>
+										<div class="col-sm-4 col-12 rps">
+											<div class="row">
+												<div class="col-12">
+													<a href="#">Get access</a>
+													<div class="icon-wrapper">
+														<i class="fa fa-bell" aria-hidden="true"></i>
+														<i class="fa fa-rss" aria-hidden="true"></i>
+													</div>
+												</div>
+												<div class="col-12">
+													<a href="#" class="contains-open-access"><i class="fa fa-unlock" aria-hidden="true"></i>Contains open access</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="row">
+										<div class="col-sm-8 col-12 journal-name">
+											<a href="#">Inkunabula</a>
+											<p class="journal-desc">In this section can be filled with something related to the journal.</p>
+										</div>
+										<div class="col-sm-4 col-12 rps">
+											<div class="row">
+												<div class="col-12">
+													<a href="#">Get access</a>
+													<div class="icon-wrapper">
+														<i class="fa fa-bell" aria-hidden="true"></i>
+														<i class="fa fa-rss" aria-hidden="true"></i>
+													</div>
+												</div>
+												<div class="col-12">
+													<a href="#" class="contains-open-access"><i class="fa fa-unlock" aria-hidden="true"></i>Contains open access</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="row">
+										<div class="col-sm-8 col-12 journal-name">
+											<a href="#">IJSYA</a>
+										</div>
+										<div class="col-sm-4 col-12 rps">
+											<div class="row">
+												<div class="col-12">
+													<a href="#">Get access</a>
+													<div class="icon-wrapper">
+														<i class="fa fa-bell" aria-hidden="true"></i>
+														<i class="fa fa-rss" aria-hidden="true"></i>
+													</div>
+												</div>
+												<div class="col-12">
+													<a href="#" class="contains-open-access"><i class="fa fa-unlock" aria-hidden="true"></i>Contains open access</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
 @endsection
--- resources/views/home/layout/footer.blade.php
+++ resources/views/home/layout/footer.blade.php
@@ -1,1 +1,80 @@
-Footer
+<footer class="container-fluid footer-top">
+	<div class="row">
+		<div class="col-12">
+			<div class="container">
+				<div class="row d-none d-sm-block">
+					<div class="col-12 d-flex justify-content-center">
+						<a href="#">Librarians</a>
+						<a href="#">Authors</a>
+						<a href="#">Publishing partners</a>
+						<a href="#">Agents</a>
+						<a href="#">Corporates</a>
+					</div>
+				</div>
+				<div class="row d-block d-sm-none">
+					<div class="col-12">
+						<a href="#">Librarians</a>
+					</div>
+					<div class="col-12">
+						<a href="#">Authors</a>
+					</div>
+					<div class="col-12">
+						<a href="#">Publishing partners</a>
+					</div>
+					<div class="col-12">
+						<a href="#">Agents</a>
+					</div>
+					<div class="col-12">
+						<a href="#">Corporates</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+<div class="container-fluid footer-medium">
+	<div class="row">
+		<div class="col-12">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 col-12">
+						<a href="#">Accessibility</a>
+						<a href="#">Our blog</a>
+						<a href="#">News</a>
+						<a href="#">Contact and help</a>
+						<a href="#">Cambridge Core legal notices</a>
+						<a href="#">Feedback</a>
+						<a href="#">Sitemap</a>
+					</div>
+					<div class="col-sm-4 col-12">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid footer-bottom">
+	<div class="row">
+		<div class="col-12">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 col-12 left-menu">
+						<div class="logo-pk">
+							<img src="https://uindatokarama.ac.id/wp-content/uploads/2021/10/cropped-cropped-uindakar2.png" class="img-fluid logo-footer">
+							<span>UIN Datokarama Palu</span>
+						</div>
+						<a href="#">Rights & Permissions</a>
+						<a href="#">Copyright</a>
+						<a href="#">Privacy Notice</a>
+						<a href="#">Privacy Notice</a>
+						<a href="#">Cookies Policy</a>
+					</div>
+					<div class="col-sm-4 col-12">
+						<button class="btn btn-sm btn-success">Feedback</button>
+						<p style="margin-top: 5px;">&copy; UIN Datokarama Palu <?php echo date('Y') ?></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
